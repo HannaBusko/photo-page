b4b052043ece631a8e0611392bfad4b0f612bedb
HannaBusko <56487177+HannaBusko@users.noreply.github.com>
2020-06-01 18:51:11 +0300

    Updates

---
+++ precache-manifest.653826ac78418efc893e2cbdb4f1b096.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1f2dd1c5d2f6d7e1e8a586ecf2a68b",
+    "url": "/photo-page/index.html"
+  },
+  {
+    "revision": "4eb77fec4a7965d9bf73",
+    "url": "/photo-page/static/css/main.bbee8b2c.chunk.css"
+  },
+  {
+    "revision": "eda7a57c78ac72fa9a24",
+    "url": "/photo-page/static/js/2.c0ade629.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/photo-page/static/js/2.c0ade629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb77fec4a7965d9bf73",
+    "url": "/photo-page/static/js/main.c8d92b51.chunk.js"
+  },
+  {
+    "revision": "f46b55f75427d84603e5",
+    "url": "/photo-page/static/js/runtime-main.ad3ce68a.js"
+  },
+  {
+    "revision": "e7d1b706949186129962e5a171432d42",
+    "url": "/photo-page/static/media/bird.e7d1b706.jpg"
+  },
+  {
+    "revision": "2ecb426f85ffc1c53b677556210e629f",
+    "url": "/photo-page/static/media/fira-sans-v10-latin-700.2ecb426f.woff2"
+  },
+  {
+    "revision": "4d2dfa0d7366eb8ba6e2d9cc5ef6e58d",
+    "url": "/photo-page/static/media/fira-sans-v10-latin-700.4d2dfa0d.woff"
+  },
+  {
+    "revision": "603e6faf87382f3b7cf37ef81d64298f",
+    "url": "/photo-page/static/media/fira-sans-v10-latin-700.603e6faf.ttf"
+  },
+  {
+    "revision": "832c99425622b1dd326dc39dc4cbd963",
+    "url": "/photo-page/static/media/fira-sans-v10-latin-700.832c9942.eot"
+  },
+  {
+    "revision": "e7cac841f261ca02458dc165de05a40e",
+    "url": "/photo-page/static/media/fira-sans-v10-latin-700.e7cac841.svg"
+  },
+  {
+    "revision": "7307389c2218fa743b42f50e6452bee6",
+    "url": "/photo-page/static/media/fira-sans-v10-latin-regular.7307389c.svg"
+  },
+  {
+    "revision": "78773521b0ffe376bc7edd8ec2a591fb",
+    "url": "/photo-page/static/media/fira-sans-v10-latin-regular.78773521.woff2"
+  },
+  {
+    "revision": "9720cc0c5575cb480360ff90655df31b",
+    "url": "/photo-page/static/media/fira-sans-v10-latin-regular.9720cc0c.eot"
+  },
+  {
+    "revision": "baa3b51e20ff84bf4964223288c0ba10",
+    "url": "/photo-page/static/media/fira-sans-v10-latin-regular.baa3b51e.woff"
+  },
+  {
+    "revision": "ff33952a9f8385a840074521efbc8f38",
+    "url": "/photo-page/static/media/fira-sans-v10-latin-regular.ff33952a.ttf"
+  },
+  {
+    "revision": "8f2ceb68f558d0ab7076fe9668c0a409",
+    "url": "/photo-page/static/media/jazz1.8f2ceb68.jpg"
+  },
+  {
+    "revision": "8646fbf369d287f85ab17c4e8f5608e8",
+    "url": "/photo-page/static/media/jazz2.8646fbf3.jpg"
+  },
+  {
+    "revision": "2ccee0d3fea0e42a3cbfddcededc0692",
+    "url": "/photo-page/static/media/jazz3.2ccee0d3.jpg"
+  },
+  {
+    "revision": "1b41463659a327e713552cf4ce7b96cd",
+    "url": "/photo-page/static/media/logo.1b414636.svg"
+  },
+  {
+    "revision": "44294ea3663eb342a26fca2f4a0c071e",
+    "url": "/photo-page/static/media/nature1.44294ea3.jpg"
+  },
+  {
+    "revision": "36b66a0876efd649d5f72e0f44642a2d",
+    "url": "/photo-page/static/media/nature2.36b66a08.jpg"
+  },
+  {
+    "revision": "f558b1b60292f85f975a851c6319ecb0",
+    "url": "/photo-page/static/media/nature3.f558b1b6.jpg"
+  },
+  {
+    "revision": "9eb214e1e4b151c1674a9360ed4dc5f7",
+    "url": "/photo-page/static/media/nature4.9eb214e1.jpg"
+  },
+  {
+    "revision": "b106524bf53a7167f7aa4e06d3199d27",
+    "url": "/photo-page/static/media/nature5.b106524b.jpg"
+  },
+  {
+    "revision": "1217995b2d8dedb33ed53a423a3ec5f0",
+    "url": "/photo-page/static/media/nothing-you-could-do-v9-latin-regular.1217995b.svg"
+  },
+  {
+    "revision": "25b819a40c092516b0cbffba3614388c",
+    "url": "/photo-page/static/media/nothing-you-could-do-v9-latin-regular.25b819a4.woff2"
+  },
+  {
+    "revision": "3b61d3a7705e0e8caaf7931bbb5df194",
+    "url": "/photo-page/static/media/nothing-you-could-do-v9-latin-regular.3b61d3a7.ttf"
+  },
+  {
+    "revision": "4f8d37e58e2a6727e427a67f86fe953f",
+    "url": "/photo-page/static/media/nothing-you-could-do-v9-latin-regular.4f8d37e5.woff"
+  },
+  {
+    "revision": "68cf38f0641e9a99244f20f4446c0ed3",
+    "url": "/photo-page/static/media/nothing-you-could-do-v9-latin-regular.68cf38f0.eot"
+  },
+  {
+    "revision": "825c6ed29bf78bada10281eed894f528",
+    "url": "/photo-page/static/media/party1.825c6ed2.jpg"
+  },
+  {
+    "revision": "c539b1f579aa74e2898c739e3f4e6d42",
+    "url": "/photo-page/static/media/party2.c539b1f5.jpg"
+  },
+  {
+    "revision": "21f15f0ba26ea58b43e69e06766a5e5d",
+    "url": "/photo-page/static/media/party3.21f15f0b.jpg"
+  },
+  {
+    "revision": "1e8736c14eeccc09e4b9fcd377127bf7",
+    "url": "/photo-page/static/media/portrait.1e8736c1.jpg"
+  },
+  {
+    "revision": "05083c6677b292cbd3c666ba972e1fd1",
+    "url": "/photo-page/static/media/ptsansnarrow-400-normal.05083c66.woff"
+  },
+  {
+    "revision": "69d91d14e9b92090326a9b249b38f829",
+    "url": "/photo-page/static/media/ptsansnarrow-400-normal.69d91d14.ttf"
+  },
+  {
+    "revision": "b7418f3265a120b9a0abbd24c95712da",
+    "url": "/photo-page/static/media/ptsansnarrow-400-normal.b7418f32.woff2"
+  },
+  {
+    "revision": "c3d845cd0f6d462453b023a173d39f3a",
+    "url": "/photo-page/static/media/ptsansnarrow-400-normal.c3d845cd.eot"
+  },
+  {
+    "revision": "61ceb28722fa338261b2e15077afe50f",
+    "url": "/photo-page/static/media/ptsansnarrow-700-normal.61ceb287.woff2"
+  },
+  {
+    "revision": "b92034346d21d574f16997c40b541cfd",
+    "url": "/photo-page/static/media/ptsansnarrow-700-normal.b9203434.woff"
+  },
+  {
+    "revision": "d074f593c058a52421ed2404d625e735",
+    "url": "/photo-page/static/media/ptsansnarrow-700-normal.d074f593.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/photo-page/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/photo-page/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/photo-page/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/photo-page/static/media/slick.f97e3bbf.svg"
+  }
+]);
